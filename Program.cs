1566770d2dc18aaf79241a7e59c4dbac2fefca99
Trevor Nichols <trevor@trfg.au>
2023-06-27 13:00:34 +1000

    Test case

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.MsSql;
+
+Console.WriteLine("Hello, World!");
+
+
+await using var server = new MsSqlBuilder().Build();
+
+await server.StartAsync();
+
+var options = new DbContextOptionsBuilder<TestContext>().UseSqlServer(server.GetConnectionString()).Options;
+
+var db = new TestContext(options);
+await db.Database.EnsureCreatedAsync();
+
+var query = db.Set<Receipt>()
+            .Select(a => new { OwnerId = a.OwnerId, a.OwnerType, Cents = a.Lines.Sum(a => a.Amount) })
+            .GroupBy(a => new { OwnerId = a.OwnerId, a.OwnerType })
+            .Select(a => new { a.Key, Total = a.Sum(t => t.Cents) });
+
+
+var r = query.ToArray();
+//"SELECT [r].[OwnerId], [r].[OwnerType], COALESCE(SUM((\r\n    SELECT COALESCE(SUM([l].[Amount]), 0)\r\n    FROM [Lines] AS [l]\r\n    WHERE [r].[Id] = [l].[ReceiptId1])), 0) AS [Total]\r\nFROM [Receipts] AS [r]\r\nGROUP BY [r].[OwnerId], [r].[OwnerType]"
+// invalid query
+
+Console.WriteLine(r.Length);
+
+public class Receipt
+{
+    public string Id { get; set; }
+    public string OwnerId { get; set; }
+    public string OwnerType { get; set; }
+    public ICollection<Lines> Lines { get; set; }
+}
+
+public class Lines
+{
+    public int Id { get; set; }
+    public int ReceiptId { get; set; }
+    public int Amount { get; set; }
+}
+
+public class TestContext : DbContext
+{
+    public DbSet<Receipt> Receipts { get; set; }
+    public TestContext(DbContextOptions options) : base(options) { }
+}
